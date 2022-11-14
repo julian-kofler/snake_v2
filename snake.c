b342f13c5626d8950c770bed4d0a845791c44119
Julian Kofler <julian.kofler@stud.hs-kempten.de>
2022-11-14 10:55:52 +0100

    umgebaut

--- snake.c
+++ snake.c
@@ -14,54 +14,86 @@ enum content
     Food = '*',
     Empty = ' ',
 };
+struct entity
+{
+    enum content content;
+    int dx; // geschwindigkeit in x-Richtung
+    int dy; // geschwindigkeit in y-Richtung
+};
 struct field
 {
-    int **arr;  // speicher für Feld
-    int width;  // Breite
-    int height; // Höhe
-    int dx;     // Schlange geschwindigkeit in x-Richtung
-    int dy;     // Schlange geschwindigkeit in y-Richtung
-    bool lost;  //=1 wenn verloren
+    struct entity **arr; // speicher für Feld
+    int width;           // Breite
+    int height;          // Höhe
+    bool lost;           //=1 wenn verloren
 };
 struct field newField(int x, int y)
 {
     struct field ret = {
-        .arr = (int **)calloc(x, sizeof(int *)),
+        .arr = (struct entity **)calloc(x, sizeof(struct entity *)),
         .width = x,
         .height = y,
-        .dx = 1,
-        .dy = 0,
         .lost = false,
     };
     for (int i = 0; i < ret.width; i++)
     {
-        ret.arr[i] = (int *)calloc(ret.height, sizeof(int));
+        ret.arr[i] = (struct entity *)calloc(ret.height, sizeof(struct entity));
     }
     return ret;
 }
-
+bool isInField(struct field *field, int x, int y)
+{
+    if (!(0 < x && x < field->height - 1))
+    {
+        return 0;
+    }
+    if (!(0 < y && y < field->width - 1))
+    {
+        return 0;
+    }
+    return 1;
+}
+void setDxDy(struct field *field, int dx, int dy)
+{
+    for (int y = 0; y < field->height; y++)
+    {
+        for (int x = 0; x < field->width; x++)
+        {
+            switch (field->arr[x][y].content)
+            {
+            case Snake_head:
+            case Snake_tail:
+                field->arr[x][y].dx = dx;
+                field->arr[x][y].dy = dy;
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
 void getMove(struct field *field) // Read Input
 {
     char input;
     input = getchar();
-    //scanf(" %c", &input);
+    // scanf(" %c", &input);
+
+    int
+        headX = 0,
+        headY = 0;
     switch (input)
     {
     case 'w':
-        field->dx = 0;
-        field->dy = -1;
+        setDxDy(field, 0, -1);
         break;
     case 's':
-        field->dx = 0;
-        field->dy = 1;
+        setDxDy(field, 0, 1);
         break;
     case 'a':
-        field->dx = -1;
-        field->dy = 0;
+        setDxDy(field, -1, 0);
         break;
     case 'd':
-        field->dx = 1;
-        field->dy = 0;
+        setDxDy(field, 1, 0);
         break;
     default:
         printf("\a"); // Piepst
@@ -76,7 +108,7 @@ void printField(struct field *field) // Zeichnet das spielfeld
     {
         for (int x = 0; x < field->width; x++)
         {
-            printf("%c", field->arr[x][y]);
+            printf("%c", field->arr[x][y].content);
         }
         printf("\n");
     }
@@ -87,7 +119,9 @@ void clearField(struct field *field) // clears Field without Borders
     {
         for (int x = 1; x < field->width - 1; x++)
         {
-            field->arr[x][y] = Empty;
+            field->arr[x][y].content = Empty;
+            field->arr[x][y].dx = 0;
+            field->arr[x][y].dy = 0;
         }
     }
 }
@@ -95,20 +129,20 @@ void createBorder(struct field *field) // Malt den Rand
 {
     for (int x = 0; x < field->width; x++)
     {
-        field->arr[x][0] = Border;
-        field->arr[x][field->height - 1] = Border;
+        field->arr[x][0].content = Border;
+        field->arr[x][field->height - 1].content = Border;
     }
     for (int y = 0; y < field->height; y++)
     {
-        field->arr[0][y] = Border;
-        field->arr[field->width - 1][y] = Border;
+        field->arr[0][y].content = Border;
+        field->arr[field->width - 1][y].content = Border;
     }
 }
 void createFood(struct field *field) // erschafft essen
 {
-    field->arr[7][7] = Food;
+    field->arr[7][7].content = Food;
 }
-void moveSnake(struct field *field)
+/*void moveSnake(struct field *field)
 {
     int Xnext, Ynext;
     int fertig = 0;
@@ -116,7 +150,7 @@ void moveSnake(struct field *field)
     {
         for (int x = 0; (!fertig) && (x < field->width); x++)
         {
-            if (field->arr[x][y] == Snake_head)
+            if (field->arr[x][y].content == Snake_head)
             {
                 Xnext = x + field->dx;
                 Ynext = y + field->dy;
@@ -144,7 +178,39 @@ void moveSnake(struct field *field)
             }
         }
     }
+}*/
+void moveAll(struct field *field)
+{
+    for (int y = 0; y < field->height; y++)
+    {
+        for (int x = 0; x < field->width; x++)
+        {
+            int
+                x_next = x + field->arr[x][y].dx,
+                y_next = y + field->arr[x][y].dy;
+
+            if (!isInField(field, x_next, y_next))
+            {
+                field->lost = 1;
+                break;
+            }
+            if (x == x_next && y == y_next)
+            {
+                continue;
+            }
+
+            if (field->arr[x_next][y_next].content == Empty)
+            {
+                field->arr[x_next][y_next].content == field->arr[x][y].content;
+            }
+            else
+            {
+                field->lost = 1;
+            }
+        }
+    }
 }
+
 int main(int argc, char const *argv[])
 {
     struct field spielfeld = newField(80, 15);
@@ -153,7 +219,8 @@ int main(int argc, char const *argv[])
     createFood(&spielfeld);
     printField(&spielfeld);
 
-    spielfeld.arr[5][5] = Snake_head;
+    spielfeld.arr[5][5].content = Snake_head;
+    spielfeld.arr[5][5].dx = 1;
 
     while (!spielfeld.lost)
     {
@@ -161,8 +228,8 @@ int main(int argc, char const *argv[])
         {
             getMove(&spielfeld);
         }
-        
-        moveSnake(&spielfeld);
+
+        moveAll(&spielfeld);
 
         printField(&spielfeld);
         usleep(500 * 1000); // 50 ms delay
